71ec411440046ba9038514e0fc1041635f291aa8
jverzosa <jverzosa@precision.com.au>
2024-05-19 20:25:06 +0800

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: DashboardComponent
+  },
+  {
+    path: 'billing',
+    loadChildren: () => {
+      return loadRemoteModule({
+        remoteEntry: "http://localhost:4100/remoteEntry.js",
+        remoteName: 'billing',
+        exposedModule: './BillingModule'
+      }).then(m => m.BillingModule).catch(error =>
+        console.log('Error=>', error));
+    }
+  },
+  {
+    path: 'reports',
+    loadChildren: () => {
+      return loadRemoteModule({
+        remoteEntry: "http://localhost:4300/remoteEntry.js",
+        remoteName: 'reports',
+        exposedModule: './ReportModule'
+      }).then(m => m.ReportModule).catch(error =>
+        console.log('Error=>', error));
+    }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { NavbarComponent } from './modules/navbar/navbar.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatListModule,
+    MatMenuModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
